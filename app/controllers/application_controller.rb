8bf3430b3f39cf50b94bb1210da9354c56ddf770
Michael <Mlcorrieri@gmail.com>
2021-05-31 19:00:24 -0400

    working fetch for host Mon 7pm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
 class ApplicationController < ActionController::API
 
     def host_loggin_in?
+       
         begin
             headers = request.headers["Authorization"]
             token = headers.split(" ")[1]
             host_id = JWT.decode(token, "todo")[0]["host_id"]
-            @host = host.find(host_id)
+            @host = Host.find(host_id)
         rescue 
             @host = nil 
         end
-        render json: {error: "Please login"} unless @host   
+
+        unless @host 
+    
+        render json: {error: "Please login"}  
+        end
        
     end
 
     def generate_token(payload)
-        JWT.encode(payload, "todo", "HS256")
+        JWT.encode(payload, "todo")
     end
 end
