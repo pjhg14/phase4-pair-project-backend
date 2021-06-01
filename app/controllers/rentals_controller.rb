20740e88000b353f4196278f0cdd6b5517822efd
Michael <Mlcorrieri@gmail.com>
2021-06-01 15:19:31 -0400

    created rental create, update, delete actions

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,6 @@
 class RentalsController < ApplicationController
+    before_action :host_loggin_in?, only: [:create, :update]
+
 
     def index
         rentals = Rental.all
@@ -9,4 +11,38 @@ class RentalsController < ApplicationController
         rental = Rental.find(params[:id])
         render json: rental
     end
+
+    def create
+        rental = Rental.new(rental_params.merge({host_id: @host.id}))
+        if rental.valid?
+            rental.save 
+            render json: rental 
+        else
+            render json: {error: "Unable to create rental", details: rental.errors.full_messages }
+        end
+    end
+
+    def update
+        rental = Rental.find(params[:id])
+        rental.assign_attributes(rental_params)
+        if rental.valid?
+            rental.save 
+            render json: rental 
+        else
+            render json: {error: "Unable to update rental", details: rental.errors.full_messages }
+        end
+    end
+
+    def destroy
+        rental = Rental.find(params[:id])
+        rental.destroy
+        render json: {message: "Rental has been deleted"}
+    end
+
+    private
+
+    def rental_params
+        params.require(:rental).permit(:host_id, :cost, :address, :max_guests, :description, :image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :rentals
+  resources :rentals, only: [:index, :show, :create, :update, :destroy]
   resources :hosts, only: [:create, :show]
 
   post "/login", to: "hosts#login"
