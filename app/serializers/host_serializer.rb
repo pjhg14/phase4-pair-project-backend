c6087d1a9360343aa2485e717ab5e52fae8a347e
Michael <Mlcorrieri@gmail.com>
2021-06-02 10:25:03 -0400

    updated seeds Wed 1030am

--- app/serializers/host_serializer.rb
+++ app/serializers/host_serializer.rb
@@ -1,5 +1,8 @@
 class HostSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone, :about
+  # has_secure_password 
 
   has_many :rentals
 end
+
+# ActiveModelSerializers.config.default_includes = '**'
